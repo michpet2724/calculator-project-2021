e583b70209b385c15074fb3aa14fe9e12364abf1
michpet2724 <michpet2724@gmail.com>
2021-07-08 18:31:53 +0000

    created dictionary

--- calorie-calculator.py
+++ calorie-calculator.py
@@ -10,8 +10,30 @@ if gender == ("male"):
 elif gender == ("female"):
     msg = "The recommended calorie intake for your group is 2,000. This may vary based on height/weight/age."
 
-#return msg
 print(msg)
 
 # Figure out amount of calories for each food group: fruits, vegetables, grains, meats, and dairy
 
+# food_groups_fruits = [
+#     {"name": "orange", "calories": "45"}
+#     #{"name": "apple", "calories": "130"}
+#     {"name": "pear", "calories": "102"}
+# ]
+
+food_groups = input("Woud you like to know the calories in different foods? ")
+
+if food_groups == "yes":
+    ans = "Great let's continue."
+    # print(food_groups_fruits)
+
+elif food_groups == "no":
+    ans = "That's fine then. Thank you for using the calorie calculator!"
+
+
+# food_groups_vegetables = [
+
+# ]
+
+# ]
+
+# print("")
